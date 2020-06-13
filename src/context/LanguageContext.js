825857be9415224dd7cb10108c3d1c9e4837cf26
Justyna Krzywda <justyna.krzywda@gmail.com>
2020-06-13 10:18:12 +0100

    cleaning

--- src/context/LanguageContext.js
+++ src/context/LanguageContext.js
@@ -2,9 +2,6 @@ import React, { Component } from "react";
 
 const Context = React.createContext("english");
 
-// purpose of this file is to create a context object and export it
-// the reason why we putting this in the sepearate file is that we can import the context object into only the component that we care about
-
 export class LanguageStore extends Component {
   state = {
     language: "english",
